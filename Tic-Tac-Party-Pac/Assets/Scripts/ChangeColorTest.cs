bb8888b73f3a343155e566b4b47248f33bd951a2
kabennet <kabennet@mtu.edu>
2020-03-18 14:38:59 -0400

    Uploading my code
    Using this branch to store my code while we update because there is some issue with pulling from master

---
+++ Tic-Tac-Party-Pac/Assets/Scripts/ChangeColorTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColorTest : MonoBehaviour
+{
+    public Button changer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int red = (int)(Random.value * 255);
+        int green = (int)(Random.value * 255);
+        int blue = (int)(Random.value * 255);
+        changer.onClick.AddListener(delegate { changeColor(red, green, blue); });
+    }
+    public void changeColor(int red, int green, int blue)
+    {
+        Color c = new Vector4(red/255f, green/255f, blue/255f, 1);
+        GetComponent<Text>().color = c;
+    }
+}

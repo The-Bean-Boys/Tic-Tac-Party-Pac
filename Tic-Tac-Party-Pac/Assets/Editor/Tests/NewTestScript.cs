5572e96b443b793f5c454276caf92df5b4986067
kabennet <kabennet@mtu.edu>
2020-03-18 21:58:12 -0400

    Added some test code
    Some test code was added, not finalized yet.

---
+++ Tic-Tac-Party-Pac/Assets/Editor/Tests/NewTestScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+
+        private Scene scene;
+
+        [SetUp]
+        public void LoadScene()
+        {
+            scene = EditorSceneManager.OpenScene("Assets/Scenes/TitleScene.unity",
+                             OpenSceneMode.Single);
+        }
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            GameObject obj = GameObject.Find("Begin Test");
+            //ColorChangerTest script = obj.GetComponent<ColorChangerTest>();
+            //Assert.IsNotNull(script);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        /*
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+        */
+    }
+}

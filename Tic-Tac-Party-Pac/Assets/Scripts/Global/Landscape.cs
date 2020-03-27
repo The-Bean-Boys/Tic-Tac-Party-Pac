92be38d496754d548a0c2eddc8028722ce71dcb1
zq-Aux <55764480+zq-Aux@users.noreply.github.com>
2020-03-27 13:26:46 -0400

    Added Orientation Global Scripts

---
+++ Tic-Tac-Party-Pac/Assets/Scripts/Global/Landscape.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landscape : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+}

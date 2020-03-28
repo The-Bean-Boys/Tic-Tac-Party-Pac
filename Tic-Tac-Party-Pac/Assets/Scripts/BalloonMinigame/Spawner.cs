8643aae9a9954d4c7b5184a4c1c79a300d5af61e
zq-Aux <55764480+zq-Aux@users.noreply.github.com>
2020-03-28 16:24:32 -0400

    Balloon Minigame Finished
    15s timer, calculates and displays winner after 3 second delay to ensure all balloons are off screen. Tie condition has a spot in the code

--- Tic-Tac-Party-Pac/Assets/Scripts/BalloonMinigame/Spawner.cs
+++ Tic-Tac-Party-Pac/Assets/Scripts/BalloonMinigame/Spawner.cs
@@ -6,12 +6,14 @@ public class Spawner : MonoBehaviour
 {
     public GameObject Balloon_b;
     public GameObject Balloon_g;
+    public GameObject winnerText;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawning", 0, 1);
-        Invoke("stopIt", 15);
+        winnerText.SetActive(false);
+        InvokeRepeating("Spawning", 0, .5f);
+        Invoke("StopIt", 15);
     }
 
     private void Spawning()
@@ -19,28 +21,44 @@ public class Spawner : MonoBehaviour
         GameObject temp;
         if (Random.Range(0, 1) == 0)
         {
-            temp = Instantiate(Balloon_b, new Vector3(Random.Range(transform.position.x - 2.5f, transform.position.x + 2f), Random.Range(transform.position.y - 3, transform.position.y), transform.position.z), Quaternion.identity);
+            temp = Instantiate(Balloon_b, new Vector3(Random.Range(transform.position.x - 2.5f, transform.position.x + 2f), Random.Range(transform.position.y - 4, transform.position.y), transform.position.z), Quaternion.identity);
             temp.GetComponent<SpriteRenderer>().sortingLayerName = "Back Middle";
             temp.transform.SetParent(gameObject.transform);
-            temp = Instantiate(Balloon_g, new Vector3(Random.Range(transform.position.x - 2f, transform.position.x + 2.5f), Random.Range(transform.position.y - 3, transform.position.y), transform.position.z), Quaternion.identity);
+            temp = Instantiate(Balloon_g, new Vector3(Random.Range(transform.position.x - 2f, transform.position.x + 2.5f), Random.Range(transform.position.y - 4, transform.position.y), transform.position.z), Quaternion.identity);
             temp.GetComponent<SpriteRenderer>().sortingLayerName = "Front Middle";
             temp.transform.SetParent(gameObject.transform);
 
         }
         else
         {
-            temp = Instantiate(Balloon_g, new Vector3(Random.Range(transform.position.x - 2f, transform.position.x + 2.5f), Random.Range(transform.position.y - 3, transform.position.y), transform.position.z), Quaternion.identity);
+            temp = Instantiate(Balloon_g, new Vector3(Random.Range(transform.position.x - 2f, transform.position.x + 2.5f), Random.Range(transform.position.y - 4, transform.position.y), transform.position.z), Quaternion.identity);
             temp.GetComponent<SpriteRenderer>().sortingLayerName = "Back Middle";
             temp.transform.SetParent(gameObject.transform);
-            temp = Instantiate(Balloon_b, new Vector3(Random.Range(transform.position.x - 2.5f, transform.position.x + 2f), Random.Range(transform.position.y - 3, transform.position.y), transform.position.z), Quaternion.identity);
+            temp = Instantiate(Balloon_b, new Vector3(Random.Range(transform.position.x - 2.5f, transform.position.x + 2f), Random.Range(transform.position.y - 4, transform.position.y), transform.position.z), Quaternion.identity);
             temp.GetComponent<SpriteRenderer>().sortingLayerName = "Front Middle";
             temp.transform.SetParent(gameObject.transform);
 
         }
     }
-    
-    private void stopIt()
+
+    private void StopIt()
     {
         CancelInvoke("Spawning");
+        Invoke("Winner", 3);
+    }
+
+    private void Winner()
+    {
+        if (BalloonManagerStatic.Xtext.getScore() > BalloonManagerStatic.Otext.getScore())
+        {
+            winnerText.GetComponent<TMPro.TextMeshPro>().text = "X Wins!";
+        } else if (BalloonManagerStatic.Xtext.getScore() < BalloonManagerStatic.Otext.getScore())
+        {
+            winnerText.GetComponent<TMPro.TextMeshPro>().text = "O Wins!";
+        } else
+        {
+            winnerText.GetComponent<TMPro.TextMeshPro>().text = "A tie?";
+        }
+        winnerText.SetActive(true);
     }
 }

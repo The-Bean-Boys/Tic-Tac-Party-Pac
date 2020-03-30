297966bbd56681c7f4ea4a5b602955fdb414ad28
Tayler Wilczynski <59892573+TaylerW@users.noreply.github.com>
2020-03-30 13:14:54 -0400

    part of tug of war minigame
    New minigame partial implementation

---
+++ Tic-Tac-Party-Pac/Assets/Scripts/TugOfWarMinigame/Button_Handler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Handler : MonoBehaviour
+{
+    // Update is called once per frame
+    public void OnClick()
+    {
+        Debug.Log("A button was clicked!");
+    }
+}

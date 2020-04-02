24df06ab2aaf9a5269c14c74dee09da81e5740b3
zq-Aux <55764480+zq-Aux@users.noreply.github.com>
2020-04-01 22:09:01 -0400

    Some flappyxo implementation

---
+++ Tic-Tac-Party-Pac/Assets/Scripts/FlappyXO/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+
+    public static GameManager Instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public Text winnerText;
+
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver
+    }
+    
+    bool gameOver = false;
+
+    public bool GameOver{ get { return gameOver; } }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        tapController.OnPlayerDied += OnPlayerDied;
+    }
+
+    void OnPlayerDied()
+    {
+        gameOver = true;
+        PlayerPrefs.SetString("WinnerFlappyXO", "x"); 
+    }
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                gameOverPage.SetActive(false);
+                startPage.SetActive(false);
+                break;
+            case PageState.Start:
+                gameOverPage.SetActive(false);
+                startPage.SetActive(true);
+                break;
+            case PageState.GameOver:
+                gameOverPage.SetActive(true);
+                startPage.SetActive(false);
+                break;
+        }
+    }
+
+
+
+    public void StartGame()
+    {
+        OnGameStarted();
+        gameOver = false;
+        SetPageState(PageState.None);
+    }
+
+}
---
+++ Tic-Tac-Party-Pac/Assets/Scripts/FlappyXO/tapController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class tapController : MonoBehaviour
+{
+
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+
+    public float tapForce = 10f;
+    public float tiltSmooth = 5f;
+    public Vector3 startPos;
+    public int id = 0;
+
+    new Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    public GameObject gameManager;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "ScoreZone")
+        {
+
+        }
+
+        if (col.gameObject.tag == "DeadZone")
+        {
+            rigidbody.simulated = false;
+            forwardRotation = downRotation;
+            OnPlayerDied(); //event sent to GameManager;
+        }
+    }
+}

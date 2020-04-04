61c5c42a1a529f7664653338945a8010d1e0ca7f
zq-Aux <55764480+zq-Aux@users.noreply.github.com>
2020-04-04 17:13:12 -0400

    Cleaned & Commented FlappyXO code
    removed deprecated code & commented the three scripts

--- Tic-Tac-Party-Pac/Assets/Scripts/FlappyXO/GameManager.cs
+++ Tic-Tac-Party-Pac/Assets/Scripts/FlappyXO/GameManager.cs
@@ -5,44 +5,54 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    // Events that will be broadcast to other scripts
     public delegate void GameDelegate();
     public static event GameDelegate OnGameStarted;
     public static event GameDelegate OnGameOver;
 
+    // Creating a static instance of this script to reference
     public static GameManager Instance;
 
+    // To figure out which bird dies first
     public GameObject xBird;
     public GameObject oBird;
 
+    // Different UI pages, startPage just has a playbutton for now,
+    //  gameOverPage has the winner (or tie) text display
     public GameObject startPage;
     public GameObject gameOverPage;
-    public Text winnerText;
+    public Text winnerText; // text to change to display winner
 
+    // Just an enum to easily reference which page is active
     enum PageState
     {
-        None,
-        Start,
-        GameOver,
-        FinalGameOver
+        None, // Game is running
+        Start, // Game is waiting to be started
+        GameOver, // Tie text is being displayed, also displays start screen
+        FinalGameOver // One bird won, no need to display start screen
     }
     
+    // Starts off the game in the game over state
     bool gameOver = true;
-
     public bool GameOver{ get { return gameOver; } }
 
+    // When this object starts running
     private void Awake()
     {
-        Instance = this;
+        Instance = this; // Make the static referece
     }
 
+    // When this object becomes enabled (after Awake, before Start(?))
     private void OnEnable()
     {
-        tapController.OnPlayerDied += OnPlayerDied;
+        TapController.OnPlayerDied += OnPlayerDied; // Subscribe to the birds' "i died" event
     }
 
+    // When disabled
     private void OnDisable()
     {
-        tapController.OnPlayerDied -= OnPlayerDied;
+        TapController.OnPlayerDied -= OnPlayerDied; // Unsubscribe to birds' "i died" event
     }
 
 
@@ -51,36 +61,36 @@ public class GameManager : MonoBehaviour
     {
         gameOver = true;
         // determines which bird won, if any
-        switch(!xBird.GetComponent<Rigidbody2D>().simulated)
+        switch(!xBird.GetComponent<Rigidbody2D>().simulated) // is xBird's physics disactivated? if yes, it hit something
         {
-            case (false):
-                if (oBird.GetComponent<Rigidbody2D>().simulated)
+            case (false): // xBird's physics still active
+                if (oBird.GetComponent<Rigidbody2D>().simulated) // if oBird's physics also active
                 {
-                    winnerText.text = "Tie! Play Again";
-                    SetPageState(PageState.GameOver);
+                    winnerText.text = "Tie! Play Again"; // set game over "winnerText" to tie. Not sure how this would happen, but just being safe
+                    SetPageState(PageState.GameOver); // Tie game over screen, (re)play button still enabled.
                 }
-                if (!oBird.GetComponent<Rigidbody2D>().simulated)
+                if (!oBird.GetComponent<Rigidbody2D>().simulated) // if oBird's physics aren't active
                 {
-                    winnerText.text = "X wins!";
-                    PlayerPrefs.SetString("WinnerFlappyXO", "x");
-                    SetPageState(PageState.FinalGameOver);
+                    winnerText.text = "X wins!"; // Only x bird's physics are active, so x didnt hit something but o did. x wins
+                    PlayerPrefs.SetString("WinnerFlappyXO", "x"); // Stores the winner of the minigame "x" under the label "WinnerFlappyXO" in PlayerPrefs 
+                    SetPageState(PageState.FinalGameOver); // Change ui page to final game over screen, because no replay needed
                 }
                 break;
-            case (true):
-                if (oBird.GetComponent<Rigidbody2D>().simulated)
+            case (true): // xBird's physics not active
+                if (oBird.GetComponent<Rigidbody2D>().simulated) // if oBird's physics are active
                 {
                     winnerText.text = "O wins!";                
                     PlayerPrefs.SetString("WinnerFlappyXO", "o");
                     SetPageState(PageState.FinalGameOver);
                 }
-                if (!oBird.GetComponent<Rigidbody2D>().simulated)
+                if (!oBird.GetComponent<Rigidbody2D>().simulated) // oBird's physics not active, both birds died at the same time
                 {
                     winnerText.text = "Tie! Play Again";
                     SetPageState(PageState.GameOver);
                 }
                 break;
         }
-        OnGameOver();
+        OnGameOver(); // Send out event notification that the game is over.
     }
 
     // changes which UI elements are active based on the pagestate
--- Tic-Tac-Party-Pac/Assets/Scripts/FlappyXO/Parallaxer.cs
+++ Tic-Tac-Party-Pac/Assets/Scripts/FlappyXO/Parallaxer.cs
@@ -8,13 +8,16 @@ using UnityEngine;
 
 public class Parallaxer : MonoBehaviour
 {
+    // Object to be Parallax'd
     class PoolObject
     {
-        public Transform transform;
-        public bool inUse;
+        public Transform transform; // Reference it's transform
+        public bool inUse; // is it on screen
+
+        // Constructor, must have transform
         public PoolObject (Transform t)
         {
-            transform = t;
+            transform = t; 
         }
         public void Use()
         {
@@ -27,46 +30,47 @@ public class Parallaxer : MonoBehaviour
     }
 
     [System.Serializable]
+    // only used by pipes, sets a Y range in which to spawn the object
     public struct YSpawnRange
     {
         public float min;
         public float max;
     }
 
-    public GameObject Prefab;
-    public int poolSize;
-    public float shiftSpeed;
-    public float spawnRate;
+    public GameObject Prefab; // Prefab to Parallax for this script
+    public int poolSize; // max objects to have loaded at once
+    public float shiftSpeed; // how quickly it scrolls across the screen
+    public float spawnRate; // how often (in seconds) to spawn a new PoolObject of the prefab
 
-    public YSpawnRange ySpawnRange;
-    public Vector3 defaultSpawnPos;
-    public bool spawnImmediate; //particle prewarm
-    public Vector3 immediateSpawnPos;
-    public Vector2 targetAspectRatio;
+    public YSpawnRange ySpawnRange; // referencable range
+    public Vector3 defaultSpawnPos; // default (off screen) spawn coords
+    public bool spawnImmediate; // should this prefab be on the screen to start? (background, not pipes)
+    public Vector3 immediateSpawnPos; // if yes ^, what coords
+    public Vector2 targetAspectRatio; // which aspect ratio are we set for?
 
-    float spawnTimer;
-    float targetAspect;
-    PoolObject[] poolObjects;
-    GameManager game;
+    float spawnTimer; 
+    float targetAspect; // actual aspect ratio decimal
+    PoolObject[] poolObjects; // Array of spawned objects
+    GameManager game; // reference to gamemanager script
 
     private void Awake()
     {
-        Configure();
+        Configure(); // set up parallax
     }
 
     private void Start()
     {
-        game = GameManager.Instance;
+        game = GameManager.Instance; //get reference to static instance
     }
 
     private void OnEnable()
     {
-        GameManager.OnGameOver += OnGameOver;
+        GameManager.OnGameOver += OnGameOver; // subscribe to event
     }
 
     private void OnDisable()
     {
-        GameManager.OnGameOver -= OnGameOver;
+        GameManager.OnGameOver -= OnGameOver; // unsubscribe
     }
 
     void OnGameOver()
@@ -74,11 +78,11 @@ public class Parallaxer : MonoBehaviour
         for (int i = 0; i < poolObjects.Length; i++)
         {
             poolObjects[i].Dispose();
-            poolObjects[i].transform.position = Vector3.one * 1000;
+            poolObjects[i].transform.position = Vector3.one * 1000; // move them off screen
         }
         if (spawnImmediate)
         {
-            SpawnImmediate();
+            SpawnImmediate(); // spawn immediate prefabs (stationary)
         }
     }
 
@@ -87,6 +91,8 @@ public class Parallaxer : MonoBehaviour
         if (game.GameOver) return;
 
         Shift();
+
+        // calculates timer to determine if its time to spawn another PoolObject
         spawnTimer += Time.deltaTime;
         if (spawnTimer > spawnRate)
         {
@@ -95,17 +101,18 @@ public class Parallaxer : MonoBehaviour
         }
     }
 
+    // initial configuration
     void Configure()
     {
         targetAspect = targetAspectRatio.x / targetAspectRatio.y;
         poolObjects = new PoolObject[poolSize];
         for (int i = 0; i < poolObjects.Length; i++)
         {
-            GameObject go = Instantiate(Prefab) as GameObject;
-            Transform t = go.transform;
-            t.SetParent(transform);
-            t.position = Vector3.one * 1000;
-            poolObjects[i] = new PoolObject(t);
+            GameObject go = Instantiate(Prefab) as GameObject; // instantiate a prefab
+            Transform t = go.transform; // get transform of newly instantiated prefab
+            t.SetParent(transform); // set new inst. prefab's parent to its spawner obj
+            t.position = Vector3.one * 1000; // move (way) off screen to start
+            poolObjects[i] = new PoolObject(t); // new poolobject with this object's transform
         }
 
         if (spawnImmediate)
@@ -114,16 +121,19 @@ public class Parallaxer : MonoBehaviour
         }
     }
 
+    // "spawn" one of the poolobjects in the default coords position
     void Spawn()
     {
         Transform t = GetPoolObject();
         if (t == null) return; //if true, the poolSize is too small
-        Vector3 pos = Vector3.zero;
-        pos.x = (defaultSpawnPos.x * Camera.main.aspect) / targetAspect;
+        Vector3 pos = Vector3.zero; // zero out coords
+        pos.x = (defaultSpawnPos.x * Camera.main.aspect) / targetAspect; // make sure x position is off screen no matter the "real" aspect ratio
         pos.y = Random.Range(ySpawnRange.min, ySpawnRange.max);
         t.position = pos;
     }
 
+    // "spawn" one of the poolobjects directly at "0,0" because it should be on screen to start
+    //  i.e. environment objects: the ground, clouds, stars
     void SpawnImmediate()
     {
         Transform t = GetPoolObject();
@@ -135,24 +145,28 @@ public class Parallaxer : MonoBehaviour
         Spawn();
     }
 
+    // move to the left by this script's shiftSpeed
     void Shift()
     {
         for (int i = 0; i < poolObjects.Length; i++)
         {
             poolObjects[i].transform.position += -Vector3.right * shiftSpeed * Time.deltaTime;
-            CheckDisposeObject(poolObjects[i]);
+            CheckDisposeObject(poolObjects[i]); // check if off screen
         }
     }
 
+    // check if poolobject off screen
     void CheckDisposeObject(PoolObject poolObject)
     {
+        // if off screen (no matter the "real" aspect)
         if (poolObject.transform.position.x < (-defaultSpawnPos.x * Camera.main.aspect) / targetAspect)
         {
-            poolObject.Dispose();
-            poolObject.transform.position = Vector3.one * 1000;
+            poolObject.Dispose(); // mark as "unused" (can be respawned "safely" -- currently off screen)
+            poolObject.transform.position = Vector3.one * 1000; // move offscreen
         }
     }
 
+    // return the transform of a currently unused poolObject
     Transform GetPoolObject()
     {
         for (int i = 0; i < poolObjects.Length; i++)

0941dfb7b54d593521ecd84e8af38fd03ae00f44
dlschoof <55069309+dlschoof@users.noreply.github.com>
2020-04-07 12:34:41 -0400

    Update GamePlay.cs for Win

--- Tic-Tac-Party-Pac/Assets/Scripts/GameScene/GamePlay.cs
+++ Tic-Tac-Party-Pac/Assets/Scripts/GameScene/GamePlay.cs
@@ -212,18 +212,15 @@ public class GamePlay : MonoBehaviour
 
         if (cat)
         {
-            CatBig();
+            if (xCount > oCount)
+            {
+                Debug.Log("Player X Wins!");
+            }
+            else
+            {
+                Debug.Log("Player O Wins!");
+            }
         }
     }
 
-    void CatBig()
-    {
-        if(xCount > oCount)
-        {
-            Debug.Log("Player X Wins!");
-        } else
-        {
-            Debug.Log("Player O Wins!");
-        }
-    }
 }

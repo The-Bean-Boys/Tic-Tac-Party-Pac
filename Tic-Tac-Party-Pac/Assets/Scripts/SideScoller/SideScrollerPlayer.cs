ad0eefea74a6924cefd52c5b63d9c296da0d19ac
zq-Aux <55764480+zq-Aux@users.noreply.github.com>
2020-04-11 14:02:09 -0400

    Some implementation for Side Scroller
    Added random generation and scrolling of obstacles, obstacle collision TBD

---
+++ Tic-Tac-Party-Pac/Assets/Scripts/SideScoller/SideScrollerPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScrollerPlayer : MonoBehaviour
+{
+    public float jumpPower = 10.0f;
+    public float posX;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * (jumpPower * rb.mass * rb.gravityScale * 20.0f));
+        }
+        //Hit in face
+        if (transform.position.x < posX)
+        {
+            GameOver();
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log(rb.velocity);
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ Tic-Tac-Party-Pac/Assets/Scripts/SideScoller/SideScrollerScroller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScrollerScroller : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    public GameObject[] challenges;
+    public float freq = 0.5f;
+    float counter = 0.0f;
+    public Transform challengesSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRandomChallenge();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GenerateObjects
+        if (counter <= 0.0f)
+        {
+            GenerateRandomChallenge();
+        } else
+        {
+            counter -= Time.deltaTime * freq;
+        }
+
+        //Scrolling
+        GameObject currentChild;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollChallenge(currentChild);
+            if (currentChild.transform.position.x <= -15)
+            {
+                Destroy(currentChild);
+            }
+        }
+
+    }
+
+
+    void ScrollChallenge(GameObject currChallenge)
+    {
+        currChallenge.transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+    }
+
+    void GenerateRandomChallenge()
+    {
+        GameObject newChallenge = Instantiate(challenges[Random.Range(0, challenges.Length)], challengesSpawnPoint.position, Quaternion.identity) as GameObject;
+        newChallenge.transform.parent = transform;
+        counter = 1.0f;
+    }
+}
